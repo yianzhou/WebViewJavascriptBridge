16639d96da190c132cecb2642acf3bb414eb9faa
yianzhou <yianzhou@tencent.com>
2022-03-20 18:17:42 +0800

    Demo

--- Example Apps/ExampleApp-iOS/ExampleWKWebViewController.m
+++ Example Apps/ExampleApp-iOS/ExampleWKWebViewController.m
@@ -27,9 +27,8 @@
     _bridge = [WebViewJavascriptBridge bridgeForWebView:webView];
     [_bridge setWebViewDelegate:self];
     
-    [_bridge registerHandler:@"testObjcCallback" handler:^(id data, WVJBResponseCallback responseCallback) {
-        NSLog(@"testObjcCallback called: %@", data);
-        responseCallback(@"Response from testObjcCallback");
+    [_bridge registerHandler:@"testJSBridge" handler:^(id data, WVJBResponseCallback responseCallback) {
+        responseCallback(data);
     }];
     
     [_bridge callHandler:@"testJavascriptHandler" data:@{ @"foo":@"before ready" }];
